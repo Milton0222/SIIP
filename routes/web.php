7da90de4da11a17cde5560b6e7409217716b353e
Eugênio Tch. Camela <chongcamela@gmail.com>
2023-09-28 16:21:18 +0100

    comentei a rota /post

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('/');
-Route::get('/post', Post::class);
+//Route::get('/post', Post::class);
 
 Route::middleware([
     'auth:sanctum',
